4f18641bf0e379af1826f80236dff937d099204b
salamandermike <salamandermike@gmail.com>
2014-08-06 12:06:07 -0700

    messing with PORT num

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ passport.deserializeUser(function (id, done){// req.logout();
 });
 
 // create process env port to be same on both local and heroku
-app.listen(80, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("NODEMON RIDING IN THE CODE-VAN localhost:3000");
 });
 
